5468efd070ed644ca923b15b78f64d15b2fd536f
quest <quest@mac.com>
2010-10-31 18:53:04 -0700

    Fix cold deploy issue

--- config/deploy/recipes/passenger.rb
+++ config/deploy/recipes/passenger.rb
@@ -4,5 +4,8 @@ Capistrano::Configuration.instance(:must_exist).load do
     task :restart, :roles => :app, :except => { :no_release => true } do
       run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
     end
+    task :start, :roles => :app, :except => { :no_release => true } do
+      run "#{try_sudo} touch #{File.join(current_path, 'tmp', 'restart.txt')}"
+    end
   end
 end
